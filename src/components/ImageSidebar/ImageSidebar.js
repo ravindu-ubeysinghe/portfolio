686a0e91658f0950353aa1e600e94495db8073cd
ravindu.ubeysinghe <ravindu.ubeysinghe@beteasy.com.au>
2019-10-10 01:07:12 +1100

    feat: added base styling

--- src/components/ImageSidebar/ImageSidebar.js
+++ src/components/ImageSidebar/ImageSidebar.js
@@ -2,10 +2,13 @@ import React from 'react';
 import styles from './ImageSidebar.module.css';
 import ProfilePic from './assets/profile-pic.png';
 
-export default function ImageSidebar({ className }) {
+export default function ImageSidebar() {
     return (
-        <div className={className}>
+        <div className={styles.imageSidebar}>
             <div className={styles.imageContainer}>
+                <h2 className={styles.titleBlock}>Hello there, I'm Ravindu
+                    <p>I'm a Web Developer based in Melbourne, Australia</p>
+                </h2>
                 <img className={styles.image} src={ProfilePic} alt="Ravindu Ubeysinghe" />
             </div>
         </div>
--- src/components/RightSidebar/RightSidebar.js
+++ src/components/RightSidebar/RightSidebar.js
@@ -1,32 +1,36 @@
 import React from 'react';
 import Accordion from 'react-bootstrap/Accordion';
 import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button'
+import Button from 'react-bootstrap/Button';
+
+import styles from './RightSidebar.module.css';
 
 export default function RightSidebar({ className }) {
     
     return (
-        <Accordion>
-        <Card>
-            <Card.Header>
-            <Accordion.Toggle as={Button} variant="link" eventKey="1">
-                Previous Projects
-            </Accordion.Toggle>
-            </Card.Header>
-            <Accordion.Collapse eventKey="1">
-            <Card.Body>Sample Projects as a list and labels</Card.Body>
-            </Accordion.Collapse>
-        </Card>
-        <Card>
-            <Card.Header>
-            <Accordion.Toggle as={Button} variant="link" eventKey="0">
-                Qualifications
-            </Accordion.Toggle>
-            </Card.Header>
-            <Accordion.Collapse eventKey="0">
-            <Card.Body>Qualifications as a list</Card.Body>
-            </Accordion.Collapse>
-        </Card>
-        </Accordion>
+        <div className={styles.rightSidebar}>
+            <Accordion>
+                <Card>
+                    <Card.Header>
+                    <Accordion.Toggle className={styles.proToggle} as={Button} variant="link" eventKey="1">
+                        Previous Projects
+                    </Accordion.Toggle>
+                    </Card.Header>
+                    <Accordion.Collapse eventKey="1">
+                    <Card.Body>Sample Projects as a list and labels</Card.Body>
+                    </Accordion.Collapse>
+                </Card>
+                <Card>
+                    <Card.Header>
+                    <Accordion.Toggle className={styles.qualToggle} as={Button} variant="link" eventKey="0">
+                        Qualifications
+                    </Accordion.Toggle>
+                    </Card.Header>
+                    <Accordion.Collapse eventKey="0">
+                    <Card.Body>Qualifications as a list</Card.Body>
+                    </Accordion.Collapse>
+                </Card>
+            </Accordion>
+        </div>
     )
 }
