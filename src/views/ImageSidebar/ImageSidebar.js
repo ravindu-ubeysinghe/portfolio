ac422ad381b4088c79d409503e3d36a90a407803
ravindu.ubeysinghe <ravindu.ubeysinghe@beteasy.com.au>
2019-10-19 17:11:35 +1100

    fix: added code formatting

--- src/views/ImageSidebar/ImageSidebar.js
+++ src/views/ImageSidebar/ImageSidebar.js
@@ -18,7 +18,7 @@ export default function ImageSidebar() {
                         <div className={styles.imageContainer}>
                             <img className={styles.image} src={ProfilePic} alt="Ravindu Ubeysinghe" />
                             <h2 className={styles.titleBlock}>Hello there!
-                                <p>I'm Ravindu, a Web Developer based in Melbourne, Australia</p>
+                                <p>I{'\''}m Ravindu, a Web Developer based in Melbourne, Australia</p>
                             </h2>
                             <div className={styles.socialBlock}>
                                 <a href={'https://www.linkedin.com/in/ravinduubeysinghe/'} rel="noopener noreferrer" target="_blank">
